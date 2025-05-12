3af568175134410e8473109682158dc7271984e4
Awwesome Fonts <211348291+awwesomefonts@users.noreply.github.com>
2025-05-12 16:27:11 +0200

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,9 +1,1 @@
-// Check if the script is being accessed directly by URL
-if (window.location.href.indexOf('integration.js') !== -1) {
-  // Prevent the content from displaying when accessed directly
-  document.write(""); // This will remove the content of the page
-  window.location.href = 'about:blank'; // Redirect to a blank page if accessed directly
-} else {
-  // Normal script functionality when included in a page
-  console.log("Site developed by TonPrénom");
-}
+console.log("Site developed by TonPrénom");
