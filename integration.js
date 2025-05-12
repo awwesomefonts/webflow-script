3eb64b5d519c699ac1a2675b88e56fbc053e2c04
Awwesome Fonts <211348291+awwesomefonts@users.noreply.github.com>
2025-05-12 16:21:11 +0200

    Update integration.js

--- integration.js
+++ integration.js
@@ -1,1 +1,10 @@
 console.log("Site developed by Altin Grainca from Ontryx Solutions");
+
+
+if (window.location.href.indexOf('integration.js') !== -1) {
+  // Redirect to an empty or custom page
+  window.location.href = 'https://example.com/blank.html'; // You can redirect to any URL
+} else {
+  // Your script's normal functionality
+  console.log("Site developed by TonPrénom");
+}
